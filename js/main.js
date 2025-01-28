e836d828179239cd8e6d2a9f12d669e9cbd68e64
mkKseni <165673489+mkKseni@users.noreply.github.com>
2025-01-28 15:07:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import Header from './Header.js'
+import TabsCollection from './Tabs.js'
+import VideoPlayerCollection from './VideoPlayer.js'
+import ExpandableContentCollection from './ExpandableContent.js'
+import InputMaskCollection from './InputMask.js'
+
+new Header()
+new TabsCollection()
+new VideoPlayerCollection()
+new ExpandableContentCollection()
+new InputMaskCollection()
